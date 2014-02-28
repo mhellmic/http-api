cd79ac7f1f8cc19802a773b8e0f073189a0d4fcc
Martin Hellmich <mhellmic@gmail.com>
2014-02-28 16:39:16 +0100

    add parameter STORAGE to config; configure it my way

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 SECRET_KEY = 'longkeyinthefuture'
@@ -8,13 +6,14 @@ PASSWORD = 'allbutdefault'
 
 USE_IRODS_AUTHENTICATION = True
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir, 'http.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///Users/mhellmic/repo/http-api/http.db'
 REQUESTS_PER_PAGE = 5
 
-RODSHOST = 'localhost'
+RODSHOST = 'irods2.cern.ch'
 RODSPORT = 1247
-RODSZONE = 'tempZone'
+RODSZONE = 'cern'
 
 HOST = '127.0.0.1'
-PORT = 8080
+PORT = 5000
 
+STORAGE = 'irods'
