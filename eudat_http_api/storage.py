99ee74ece71b9a55114b3d15e477a3adfac28a27
Martin Hellmich <mhellmic@gmail.com>
2014-03-04 14:02:15 +0100

    adjust storage.py to 4spaces

--- eudat_http_api/storage.py
+++ eudat_http_api/storage.py
@@ -5,12 +5,12 @@ from __future__ import with_statement
 from eudat_http_api import app
 
 if app.config['STORAGE'] == 'local':
-  app.logger.debug('using local storage backend')
-  from eudat_http_api.localstorage import *
+    app.logger.debug('using local storage backend')
+    from eudat_http_api.localstorage import *
 
 elif app.config['STORAGE'] == 'irods':
-  app.logger.debug('using irods storage backend')
-  from eudat_http_api.irodsstorage import *
+    app.logger.debug('using irods storage backend')
+    from eudat_http_api.irodsstorage import *
 
 else:
-  raise NotImplementedError("%s does not exist" % app.config['STORAGE'])
+    raise NotImplementedError("%s does not exist" % app.config['STORAGE'])
