30366e0ed0ff02f32fc6c42d0b1bc398415d6797
httpPrincess <ryba271+git@gmail.com>
2014-03-06 13:32:35 +0100

    applying styling guideliness

--- eudat_http_api/routes.py
+++ eudat_http_api/routes.py
@@ -25,16 +25,17 @@ Subtle, but vicious.
 from __future__ import with_statement
 from datetime import datetime
 
+import flask
+from flask import request
+from flask import json
+from flask import abort, url_for
+
 from eudat_http_api import app, db
 from eudat_http_api import registration_worker
 from eudat_http_api import invenioclient
 from eudat_http_api import auth
 from eudat_http_api import cdmi
 from models import RegistrationRequest, RegistrationRequestSerializer
-import flask
-from flask import request
-from flask import json
-from flask import redirect, abort, url_for
 from config import REQUESTS_PER_PAGE
 
 # it seems not to be possible to send
@@ -42,43 +43,48 @@ from config import REQUESTS_PER_PAGE
 #from multiprocessing import Process
 from threading import Thread
 
-import requests
-
-
 
 def request_wants_json():
-  """from http://flask.pocoo.org/snippets/45/"""
-  best = request.accept_mimetypes.best_match(['application/json', 'text/html'])
-  return best == 'application/json' and \
-      request.accept_mimetypes[best] > \
-      request.accept_mimetypes['text/html']
+    """from http://flask.pocoo.org/snippets/45/"""
+    best = request.accept_mimetypes.best_match(['application/json', 'text/html'])
+    return best == 'application/json' and \
+           request.accept_mimetypes[best] > \
+           request.accept_mimetypes['text/html']
+
+
+def get_hal_links(reg_requests, page):
+    """returns links in json hal format"""
+    navi = dict()
+    navi['self'] = {'href': url_for('get_requests', page=page)}
+    if reg_requests.has_next:
+        navi['next'] = {'href': url_for('get_requests', page=reg_requests.next_num)}
+    if reg_requests.has_prev:
+        navi['prev'] = {'href': url_for('get_requests', page=reg_requests.prev_num)}
+
+    return navi
 
 
 @app.route('/request/', methods=['GET'])
 @auth.requires_auth
 def get_requests():
-  """Get a requests list."""
-  page = int(request.args.get('page', '1'))
-  requests = RegistrationRequest.query.order_by(RegistrationRequest.timestamp.desc()).paginate(page, REQUESTS_PER_PAGE, False)
+    """Get a requests list."""
+    page = int(request.args.get('page', '1'))
+    reg_requests = RegistrationRequest.query.order_by(RegistrationRequest.timestamp.desc()).paginate(page,
+                                                                                                     REQUESTS_PER_PAGE,
+                                                                                                     False)
 
-  #links in hal standard
-  navi = dict()
-  navi['self'] = {'href':url_for('get_requests', page=page)}
-  if requests.has_next:
-      navi['next'] = {'href':url_for('get_requests', page=requests.next_num)}
-  if requests.has_prev:
-      navi['prev'] = {'href':url_for('get_requests', page=requests.prev_num)}
+    if request_wants_json():
+        return flask.jsonify(
+            {"requests": RegistrationRequestSerializer(reg_requests.items, many=True).data,
+             "_links": get_hal_links(reg_requests, page)})
 
-  if request_wants_json():
-      return flask.jsonify({"requests": RegistrationRequestSerializer(requests.items, many=True).data, "_links": navi})
-
-  return flask.render_template('requests.html', requests=requests)
+    return flask.render_template('requests.html', requests=reg_requests)
 
 
 @app.route('/request/', methods=['POST'])
 @auth.requires_auth
 def post_request():
-  """Submit a new registration request
+    """Submit a new registration request
 
   Specify in the message body:
   src: url of the source file
@@ -87,45 +93,45 @@ def post_request():
   The function returns a URL to check the status of the request.
   The URL includes a request ID.
   """
-  app.logger.debug('Entering post_request()')
+    app.logger.debug('Entering post_request()')
 
-  req_body = None
-  if flask.request.headers.get('Content-Type') == 'application/json':
-    req_body = json.loads(flask.request.data)
-  else:
-    req_body = flask.request.form
+    req_body = None
+    if flask.request.headers.get('Content-Type') == 'application/json':
+        req_body = json.loads(flask.request.data)
+    else:
+        req_body = flask.request.form
 
-  src_url = req_body['src_url']
-  #TODO: check if src is a valid URL
-  r = RegistrationRequest(src_url=src_url, status_description='W', timestamp=datetime.utcnow())
-  db.session.add(r)
-  db.session.commit()
+    src_url = req_body['src_url']
+    #TODO: check if src is a valid URL
+    r = RegistrationRequest(src_url=src_url, status_description='W', timestamp=datetime.utcnow())
+    db.session.add(r)
+    db.session.commit()
 
-  # start worker
-  p=Thread(target=registration_worker.register_data_object,
-             args=(r.id))
-  p.start()
+    # start worker
+    p = Thread(target=registration_worker.register_data_object,
+               args=r.id)
+    p.start()
 
-  if request_wants_json():
-    return flask.jsonify(request_id=r.id), 201
-  else:
-    return flask.render_template('requestcreated.html',request=r), 201
+    if request_wants_json():
+        return flask.jsonify(request_id=r.id), 201
+    else:
+        return flask.render_template('requestcreated.html', reg=r), 201
 
 
 @app.route('/request/<request_id>', methods=['GET'])
 @auth.requires_auth
 def get_request(request_id):
-  """Poll the status of a request by ID."""
-  r = RegistrationRequest.query.get(request_id)
+    """Poll the status of a request by ID."""
+    r = RegistrationRequest.query.get(request_id)
 
-  #TODO: json error?
-  if r is None:
-      return abort(404)
+    #TODO: json error?
+    if r is None:
+        return abort(404)
 
-  if request_wants_json():
-      return flask.jsonify({'request': RegistrationRequestSerializer(r).data})
+    if request_wants_json():
+        return flask.jsonify({'request': RegistrationRequestSerializer(r).data})
 
-  return flask.render_template('singleRequest.html', r=r)
+    return flask.render_template('singleRequest.html', r=r)
 
 
 #### /registered container ####
@@ -134,31 +140,30 @@ def get_request(request_id):
 @app.route('/registered/<pid_prefix>/', methods=['GET'])
 @auth.requires_auth
 def get_pids_by_prefix():
+    # search PIDs with this prefix on handle.net
 
-  # search PIDs with this prefix on handle.net
-
-  # return list of PIDs
-  # (with links to /registered/<full_pid>) to download
-  pass
+    # return list of PIDs
+    # (with links to /registered/<full_pid>) to download
+    pass
 
 
 @app.route('/registered/<pid_prefix>/<pid_suffix>', methods=['GET'])
 @auth.requires_auth
 def get_pid_by_handle(pid_prefix, pid_suffix):
-  """Retrieves a data object by PID."""
-  pid = pid_prefix + '/' + pid_suffix
+    """Retrieves a data object by PID."""
+    pid = pid_prefix + '/' + pid_suffix
 
-  if 'metadata' in flask.request.args:
-    invenioclient.get_metadata(pid)
+    if 'metadata' in flask.request.args:
+        invenioclient.get_metadata(pid)
 
-  # resolve PID
+    # resolve PID
 
-  # extract link to data object
+    # extract link to data object
 
-  # choose link to data object
+    # choose link to data object
 
-  # return data object
-  return 'nothing there, baeh!'
+    # return data object
+    return 'nothing there, baeh!'
 
 
 #### internally used CDMI requests ####
@@ -172,30 +177,30 @@ def get_pid_by_handle(pid_prefix, pid_suffix):
 @app.route('/<path:objpath>', methods=['GET'])
 @auth.requires_auth
 def get_cdmi_obj(objpath='/'):
-  absolute_objpath = cdmi.make_absolute_path(objpath)
-  if absolute_objpath[-1] == '/':
-    return cdmi.get_cdmi_dir_obj(absolute_objpath)
-  else:
-    return cdmi.get_cdmi_file_obj(absolute_objpath)
+    absolute_objpath = cdmi.make_absolute_path(objpath)
+    if absolute_objpath[-1] == '/':
+        return cdmi.get_cdmi_dir_obj(absolute_objpath)
+    else:
+        return cdmi.get_cdmi_file_obj(absolute_objpath)
 
 
 @app.route('/', methods=['PUT'])
 @app.route('/<path:objpath>', methods=['PUT'])
 @auth.requires_auth
 def put_cdmi_obj(objpath):
-  absolute_objpath = cdmi.make_absolute_path(objpath)
-  if absolute_objpath[-1] == '/':
-    return cdmi.put_cdmi_dir_obj(absolute_objpath)
-  else:
-    return cdmi.put_cdmi_file_obj(absolute_objpath)
+    absolute_objpath = cdmi.make_absolute_path(objpath)
+    if absolute_objpath[-1] == '/':
+        return cdmi.put_cdmi_dir_obj(absolute_objpath)
+    else:
+        return cdmi.put_cdmi_file_obj(absolute_objpath)
 
 
 @app.route('/', methods=['DELETE'])
 @app.route('/<path:objpath>', methods=['DELETE'])
 @auth.requires_auth
 def del_cdmi_obj(objpath):
-  absolute_objpath = cdmi.make_absolute_path(objpath)
-  if absolute_objpath[-1] == '/':
-    return cdmi.del_cdmi_dir_obj(absolute_objpath)
-  else:
-    return cdmi.del_cdmi_file_obj(absolute_objpath)
+    absolute_objpath = cdmi.make_absolute_path(objpath)
+    if absolute_objpath[-1] == '/':
+        return cdmi.del_cdmi_dir_obj(absolute_objpath)
+    else:
+        return cdmi.del_cdmi_file_obj(absolute_objpath)
