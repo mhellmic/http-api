b0e238d8f452974e35a6525169048701c4614ed3
Martin Hellmich <mhellmic@gmail.com>
2014-03-18 11:36:33 +0100

    adapt localstorage test to app factory and nosetests
    adapt the localstorage tests so that they work with the
    create_app function. This means that we have to use local
    imports in every test and run it in an explicit app_context,
    because the sanitize function takes options from the config.
    On the way, the moduel is adapted to nosetests.

--- test/localstoragetests.py
+++ test/localstoragetests.py
@@ -1,52 +1,66 @@
 import os
-import unittest
-from eudat_http_api import app
-from eudat_http_api import localstorage
+from eudat_http_api import create_app
+
+BASE_PATH = '/tmp/'
+STORAGE = 'local'
+DEBUG = False
+TESTING = True
 
 
-class LocalStorageTest(unittest.TestCase):
-    BASE_PATH = '/tmp/'
+class TestLocalStorage:
 
-    def setUp(self):
-        app.config['BASE_PATH'] = LocalStorageTest.BASE_PATH
-        try:
-            localstorage.rmdir('/foo')
-        except OSError:
-            pass
+    def setup(self):
+        app = create_app(__name__)
+        self.app = app
 
-    def tearDown(self):
-        try:
-            localstorage.rmdir('/foo')
-        except OSError:
-            pass
+        with self.app.app_context():
+            try:
+                from eudat_http_api.http_storage import localstorage
+                localstorage.rmdir('/foo')
+            except OSError:
+                pass
+
+        self.client = app.test_client()
+
+    def teardown(self):
+        with self.app.app_context():
+            try:
+                from eudat_http_api.http_storage import localstorage
+                localstorage.rmdir('/foo')
+            except OSError:
+                pass
 
     def test_pathsant(self):
-        paths = dict()
-        paths['../../etc'] = LocalStorageTest.BASE_PATH
-        paths['a/b/c'] = os.path.join(LocalStorageTest.BASE_PATH, 'a/b/c')
-        paths['/foo'] = os.path.join(LocalStorageTest.BASE_PATH, 'foo')
+        with self.app.app_context():
+            from eudat_http_api.http_storage import localstorage
 
-        for test, result in paths.items():
-            print 'Testing %s expected result %s' % (test, result)
-            print 'Result is %s ' % localstorage.sanitize_path(test)
-            assert localstorage.sanitize_path(test) == result
+            paths = dict()
+            paths['../../etc'] = BASE_PATH
+            paths['a/b/c'] = os.path.join(BASE_PATH, 'a/b/c')
+            paths['/foo'] = os.path.join(BASE_PATH, 'foo')
 
+            for test, result in paths.items():
+                print 'Testing %s expected result %s' % (test, result)
+                print 'Result is %s ' % localstorage.sanitize_path(test)
+                assert localstorage.sanitize_path(test) == result
 
     def test_mkdir(self):
-        orglist = localstorage.ls('/')
-        fooExist = [i for i in orglist if i.name == 'foo']
-        localstorage.mkdir('/foo')
-        listing = localstorage.ls('/')
-        assert len(listing) == len(orglist) + 1
-        fooExist = [i for i in listing if i.name == 'foo']
-        assert len(fooExist) == 1
+        with self.app.app_context():
+            from eudat_http_api.http_storage import localstorage
 
-        localstorage.rmdir('/foo')
+            orglist = localstorage.ls('/')
+            fooExist = [i for i in orglist if i.name == 'foo']
+            localstorage.mkdir('/foo')
+            listing = localstorage.ls('/')
+            assert len(listing) == len(orglist) + 1
+            fooExist = [i for i in listing if i.name == 'foo']
+            assert len(fooExist) == 1
+
+            localstorage.rmdir('/foo')
 
     def test_stat(self):
-        res = localstorage.stat('/')
-        assert 'ID' in res
+        with self.app.app_context():
+            from eudat_http_api.http_storage import localstorage
 
-
-if __name__ == '__main__':
-    unittest.main()
+            res = localstorage.stat('/')
+            assert 'ID' in res
