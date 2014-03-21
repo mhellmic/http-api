144cd6eb52b117915c8dc940f20b7ee2384a8662
Martin Hellmich <mhellmic@gmail.com>
2014-03-21 11:38:00 +0100

    refactor unit tests
    there is now one place to define the URLs to be tested.
    they are run through all test functions, GET, PUT, and DELETE.
    
    sometimes there are different tests for files and folders,
    and for existent and non-existent files, because the tests
    need to make very different checks.

--- eudat_http_api/http_storage/mockstorage.py
+++ eudat_http_api/http_storage/mockstorage.py
@@ -13,10 +13,10 @@ from eudat_http_api.http_storage.storage_common import *
     testname, testpass
 
     /                       children: 1
-    /testfile               data: abc, size: 3
-    /testfolder             children: 1
-    /testfolder/testfile    data: abcdefghijklmnopqrstuvwxyz, size: 26
-    /emptyfolder            children: 0
+    /tmp/testfile               data: abc, size: 3
+    /tmp/testfolder             children: 1
+    /tmp/testfolder/testfile    data: abcdefghijklmnopqrstuvwxyz, size: 26
+    /tmp/emptyfolder            children: 0
 """
 
 
@@ -25,15 +25,15 @@ def authenticate(username, password):
 
 
 def stat(path, metadata=None):
-    if path == '/testfile':
+    if path == '/tmp/testfile':
         return {'size': 3, }
-    elif path == '/testfolder/testfile':
+    elif path == '/tmp/testfolder/testfile':
         return {'size': 26, }
     elif path == '/':
         return {'children': 1, }
-    elif path == '/testfolder' or path == '/testfolder/':
+    elif path == '/tmp/testfolder' or path == '/tmp/testfolder/':
         return {'children': 1, }
-    elif path == '/emptyfolder' or path == '/emptyfolder/':
+    elif path == '/tmp/emptyfolder' or path == '/tmp/emptyfolder/':
         return {'children': 0, }
     else:
         raise NotFoundException('Path does not exist or is not a file')
@@ -48,12 +48,12 @@ def set_user_metadata(path, user_metadata):
 
 
 def read(path, range_list=[]):
-    if path == '/testfile':
+    if path == '/tmp/testfile':
         def gen():
             yield (False, 0, 3, 'abc')
 
         return (gen(), 3, 3, [])
-    elif path == '/testfolder/testfile':
+    elif path == '/tmp/testfolder/testfile':
         def gen():
             yield (False, 0, 10, 'abcdefghij')
             yield (False, 11, 20, 'klmnopqrst')
@@ -62,44 +62,46 @@ def read(path, range_list=[]):
         return (gen(), 26, 26, [])
     elif path == '/':
         raise IsDirException('Path is a directory')
-    elif path == '/testfolder' or path == '/testfolder/':
+    elif path == '/tmp/testfolder' or path == '/tmp/testfolder/':
         raise IsDirException('Path is a directory')
-    elif path == '/emptyfolder' or path == '/emptyfolder/':
+    elif path == '/tmp/emptyfolder' or path == '/tmp/emptyfolder/':
         raise IsDirException('Path is a directory')
     else:
         raise NotFoundException('Path does not exist or is not a file')
 
 
 def write(path, stream_gen):
-    if path == '/testfile':
+    if path == '/tmp/testfile':
         raise ConflictException('Path exists')
-    elif path == '/testfolder/testfile':
+    elif path == '/tmp/testfolder/testfile':
         raise ConflictException('Path exists')
     elif path == '/':
         raise ConflictException('Path exists')
-    elif path == '/testfolder' or path == '/testfolder/':
+    elif path == '/tmp/testfolder' or path == '/tmp/testfolder/':
         raise ConflictException('Path exists')
-    elif path == '/emptyfolder' or path == '/emptyfolder/':
+    elif path == '/tmp/emptyfolder' or path == '/tmp/emptyfolder/':
         raise ConflictException('Path exists')
     elif re.match('^/\w+$', path) is not None:
         return len(list(chain(*stream_gen)))
-    elif re.match('^/testfolder/\w+$', path) is not None:
+    elif re.match('^/tmp/\w+$', path) is not None:
+        return len(list(chain(*stream_gen)))
+    elif re.match('^/tmp/testfolder/\w+$', path) is not None:
         return len(list(chain(*stream_gen)))
     else:
         raise NotFoundException('Path does not exist or is not a file')
 
 
 def ls(path):
-    if path == '/testfile':
+    if path == '/tmp/testfile':
         raise IsFileException('Path is not a directory')
-    elif path == '/testfolder/testfile':
+    elif path == '/tmp/testfolder/testfile':
         raise IsFileException('Path is not a directory')
     elif path == '/':
-        yield StorageDir('testfolder', '/testfolder')
-        yield StorageFile('testfile', '/testfile')
-    elif path == '/testfolder' or path == '/testfolder/':
-        yield StorageFile('testfile', '/testfolder/testfile')
-    elif path == '/emptyfolder' or path == '/emptyfolder/':
+        yield StorageDir('testfolder', '/tmp/testfolder')
+        yield StorageFile('testfile', '/tmp/testfile')
+    elif path == '/tmp/testfolder' or path == '/tmp/testfolder/':
+        yield StorageFile('testfile', '/tmp/testfolder/testfile')
+    elif path == '/tmp/emptyfolder' or path == '/tmp/emptyfolder/':
         for i in []:  # be an empty generator
             yield i
     else:
@@ -107,49 +109,51 @@ def ls(path):
 
 
 def mkdir(path):
-    if path == '/testfile':
+    if path == '/tmp/testfile':
         raise ConflictException('Path exists')
-    elif path == '/testfolder/testfile':
+    elif path == '/tmp/testfolder/testfile':
         raise ConflictException('Path exists')
     elif path == '/':
         raise ConflictException('Path exists')
-    elif path == '/testfolder' or path == '/testfolder/':
+    elif path == '/tmp/testfolder' or path == '/tmp/testfolder/':
         raise ConflictException('Path exists')
-    elif path == '/emptyfolder' or path == '/emptyfolder/':
+    elif path == '/tmp/emptyfolder' or path == '/tmp/emptyfolder/':
         raise ConflictException('Path exists')
     elif re.match('/\w+/?', path) is not None:
         return True, ''
-    elif re.match('/testfolder/\w+/?', path) is not None:
+    elif re.match('/tmp/\w+/?', path) is not None:
+        return True, ''
+    elif re.match('/tmp/testfolder/\w+/?', path) is not None:
         return True, ''
     else:
         raise NotFoundException('Path does not exist or is not a file')
 
 
 def rm(path):
-    if path == '/testfile':
+    if path == '/tmp/testfile':
         return True, ''
-    elif path == '/testfolder/testfile':
+    elif path == '/tmp/testfolder/testfile':
         return True, ''
     elif path == '/':
         raise IsDirException('Path is not a file')
-    elif path == '/testfolder' or path == '/testfolder/':
+    elif path == '/tmp/testfolder' or path == '/tmp/testfolder/':
         raise IsDirException('Path is not a file')
-    elif path == '/emptyfolder' or path == '/emptyfolder/':
+    elif path == '/tmp/emptyfolder' or path == '/tmp/emptyfolder/':
         raise IsDirException('Path is not a file')
     else:
         raise NotFoundException('Path does not exist or is not a file')
 
 
 def rmdir(path):
-    if path == '/testfile':
+    if path == '/tmp/testfile':
         raise IsFileException('Path is not a directory')
-    elif path == '/testfolder/testfile':
+    elif path == '/tmp/testfolder/testfile':
         raise IsFileException('Path is not a directory')
     elif path == '/':
         return True, ''
-    elif path == '/testfolder' or path == '/testfolder/':
+    elif path == '/tmp/testfolder' or path == '/tmp/testfolder/':
         return True, ''
-    elif path == '/emptyfolder' or path == '/emptyfolder/':
+    elif path == '/tmp/emptyfolder' or path == '/tmp/emptyfolder/':
         return True, ''
     else:
         raise NotFoundException('Path does not exist or is not a directory')
--- test/unittests.py
+++ test/unittests.py
@@ -5,7 +5,7 @@ import os
 import re
 import tempfile
 
-from mock import patch
+#from mock import patch
 from eudat_http_api import create_app
 
 db_fd, db_filename = tempfile.mkstemp()
@@ -43,14 +43,14 @@ class TestHttpRegisterApi:
             return self.client.open(url, method=method,
                                     headers=headers)
 
-    def test_requestsdb_empty_html(self):
-        with patch('eudat_http_api.auth.check_auth', return_value=True):
-            rv = self.open_with_auth('/request/', 'GET',
-                                     'testname', 'testpass')
-
-            assert rv.status_code == 200
-            # make sure that the requests list is empty
-            #assert re.search('<ul>\s*</ul>', rv.data) is not None
+#    def test_requestsdb_empty_html(self):
+#        with patch('eudat_http_api.auth.check_auth', return_value=True):
+#            rv = self.open_with_auth('/request/', 'GET',
+#                                     'testname', 'testpass')
+#
+#            assert rv.status_code == 200
+#            # make sure that the requests list is empty
+#            #assert re.search('<ul>\s*</ul>', rv.data) is not None
 
     # the mocking does not work with thread dispatching of the
     # registration worker. Disable this test as we most likely
@@ -71,10 +71,51 @@ class TestHttpRegisterApi:
 
 
 class TestHttpStorageApi:
+    ContainerType = 'dir'
+    FileType = 'file'
 
     def setup(self):
         app = create_app(__name__)
         self.client = app.test_client()
+
+    def assert_html_response(self, rv):
+        assert rv.content_type.startswith('text/html')
+        assert rv.mimetype == 'text/html'
+
+    def url_list(self):
+        l = [
+            ('/', self.ContainerType, {
+                'children': 3,
+            }, True, True),
+            ('/tmp/testfile', self.FileType, {
+                'size': 3,
+                'content': 'abc',
+            }, True, True),
+            ('/tmp/testfolder', self.ContainerType, {
+                'children': 1,
+            }, True, True),
+            ('/tmp/testfolder/', self.ContainerType, {}, True, True),
+            ('/tmp/testfolder/testfile', self.FileType, {
+                'size': 26,
+                'content': 'abcdefghijklmnopqrstuvwxyz',
+            }, True, True),
+            ('/tmp/emptyfolder', self.ContainerType, {
+                'children': 0,
+            }, True, True),
+            ('/tmp/nonfolder', self.ContainerType, {}, False, True),
+            ('/tmp/testfolder/nonfolder', self.ContainerType, {}, False, True),
+            ('/tmp/newfolder/newfile', self.ContainerType, {}, False, False),
+            ('/nonofile', self.FileType, {
+                'size': 10,
+                'content': '1234567890',
+            }, False, True),
+            ('/wrongfilesizefile', self.FileType, {
+                'size': 4444,
+                'content': '1234567890',
+            }, False, True),
+        ]
+        print 'Testing %d different URLs' % len(l)
+        return l
 
     # from https://gist.github.com/jarus/1160696
     def open_with_auth(self, url, method, username, password, data=None):
@@ -90,141 +131,145 @@ class TestHttpStorageApi:
             return self.client.open(url, method=method,
                                     headers=headers)
 
-    def test_html_folder_get(self):
-        rv = self.open_with_auth('/', 'GET',
-                                 'testname', 'testpass')
+    def check_html(self, check_func):
+        for url, objtype, objinfo, exists, parent_exists in self.url_list():
+            yield (check_func, url, objtype,
+                   objinfo, exists, parent_exists)
 
-        assert rv.status_code == 200
-        # check that there is a list with only list items
-        assert re.search('<ul>\s*(<li>.*</li>\s*)*</ul>',
-                         rv.data, re.DOTALL) is not None
-        # check that there are at least two items ('.' and '..')
-        assert re.search(
-            '<ul>\s*<li>.*\..*</li>\s*<li>.*\.\..*</li>.*</ul>',
-            rv.data, re.DOTALL) is not None
+    def test_html_get(self):
+        for t in self.check_html(self.check_html_get):
+            yield t
 
-        rv = self.open_with_auth('/testfolder', 'GET',
-                                 'testname', 'testpass')
+    def test_html_put(self):
+        for t in self.check_html(self.check_html_put):
+            yield t
 
-        assert rv.status_code == 302
-        assert rv.headers.get('Location') == 'http://localhost/testfolder/'
-        assert rv.content_type.startswith('text/html')
-        assert rv.mimetype == 'text/html'
+    def test_html_del(self):
+        for t in self.check_html(self.check_html_del):
+            yield t
 
-        rv = self.open_with_auth('/testfolder/', 'GET',
-                                 'testname', 'testpass')
+    def check_html_get(self, url, objtype, objinfo, exists, *args):
+        if objtype == self.ContainerType and exists:
+            self.check_html_folder_get(url, objinfo)
+        elif objtype == self.ContainerType and not exists:
+            self.check_html_folder_get_404(url, objinfo)
+        elif objtype == self.FileType and exists:
+            self.check_html_file_get(url, objinfo)
+        elif objtype == self.FileType and not exists:
+            self.check_html_file_get_404(url, objinfo)
 
-        assert rv.status_code == 200
-        assert rv.content_type.startswith('text/html')
-        assert rv.mimetype == 'text/html'
-        # check that there is a list with only list items
-        assert re.search('<ul>\s*(<li>.*</li>\s*)*</ul>',
-                         rv.data, re.DOTALL) is not None
-        # check that there are at least two items ('.' and '..')
-        assert re.search(
-            '<ul>\s*<li>.*\..*</li>\s*<li>.*\.\..*</li>.*</ul>',
-            rv.data, re.DOTALL) is not None
+    def check_html_put(self, url, objtype, *args):
+        if objtype == self.ContainerType:
+            self.check_html_folder_put(url, *args)
+        elif objtype == self.FileType:
+            self.check_html_file_put(url, *args)
 
-    def test_html_folder_get_404(self):
-        rv = self.open_with_auth('/nonfolder', 'GET',
-                                 'testname', 'testpass')
+    def check_html_del(self, url, objtype, objinfo, exists, *args):
+        if objtype == self.FileType:
+            self.check_html_file_del(url, objinfo, exists, *args)
+        elif objtype == self.ContainerType and exists:
+            self.check_html_folder_del(url, objinfo, exists, *args)
+        elif objtype == self.ContainerType and not exists:
+            self.check_html_folder_del_404(url, objinfo, exists, *args)
 
-        assert rv.status_code == 404
-        assert rv.content_type.startswith('text/html')
-        assert rv.mimetype == 'text/html'
+    def check_html_folder_get(self, url, objinfo):
+        if url[-1] != '/':
+            rv = self.open_with_auth(url, 'GET',
+                                     'testname', 'testpass')
 
-    def test_html_folder_delete(self):
-        rv = self.open_with_auth('/emptyfolder', 'DELETE',
-                                 'testname', 'testpass')
+            assert rv.status_code == 302
+            assert rv.headers.get('Location') == 'http://localhost%s/' % url
+            self.assert_html_response(rv)
+        else:
+            rv = self.open_with_auth(url, 'GET',
+                                     'testname', 'testpass')
 
-        assert rv.status_code == 302
-        assert rv.headers.get('Location') == 'http://localhost/emptyfolder/'
-        assert rv.content_type.startswith('text/html')
-        assert rv.mimetype == 'text/html'
+            assert rv.status_code == 200
+            self.assert_html_response(rv)
+            # check that there is a list with only list items
+            assert re.search('<ul>\s*(<li>.*</li>\s*)*</ul>',
+                             rv.data, re.DOTALL) is not None
+            # check that there are at least two items ('.' and '..')
+            assert re.search(
+                '<ul>\s*<li>.*\..*</li>\s*<li>.*\.\..*</li>.*</ul>',
+                rv.data, re.DOTALL) is not None
 
-        rv = self.open_with_auth('/emptyfolder/', 'DELETE',
-                                 'testname', 'testpass')
-
-        assert rv.status_code == 204
-        assert rv.content_type.startswith('text/html')
-        assert rv.mimetype == 'text/html'
-
-    def test_html_folder_delete_404(self):
-        rv = self.open_with_auth('/nonfolder', 'DELETE',
-                                 'testname', 'testpass')
-
-        assert rv.status_code == 404
-        assert rv.content_type.startswith('text/html')
-        assert rv.mimetype == 'text/html'
-
-    def test_html_file_get(self):
-        rv = self.open_with_auth('/testfile', 'GET',
-                                 'testname', 'testpass')
-
-        assert rv.status_code == 200
-        assert rv.content_type.startswith('text/html')
-        assert rv.mimetype == 'text/html'
-
-        assert rv.content_length == 3
-        assert rv.data == 'abc'
-
-        rv = self.open_with_auth('/testfolder/testfile', 'GET',
-                                 'testname', 'testpass')
-
-        assert rv.status_code == 200
-        assert rv.content_type.startswith('text/html')
-        assert rv.mimetype == 'text/html'
-
-        assert rv.content_length == 26
-        assert rv.data == 'abcdefghijklmnopqrstuvwxyz'
-
-    def test_html_file_get_404(self):
-        rv = self.open_with_auth('/testfolder/nonfile', 'GET',
+    def check_html_folder_get_404(self, url, objinfo):
+        rv = self.open_with_auth(url, 'GET',
                                  'testname', 'testpass')
 
         assert rv.status_code == 404
-        assert rv.content_type.startswith('text/html')
-        assert rv.mimetype == 'text/html'
+        self.assert_html_response(rv)
 
-    def test_html_file_put(self):
-        rv = self.open_with_auth('/newfile', 'PUT',
+    def check_html_file_get(self, url, fileinfo):
+        rv = self.open_with_auth(url, 'GET',
+                                 'testname', 'testpass')
+
+        assert rv.status_code == 200
+        self.assert_html_response(rv)
+
+        assert rv.content_length == fileinfo['size']
+        assert rv.data == fileinfo['content']
+
+    def check_html_file_get_404(self, url, fileinfo):
+        rv = self.open_with_auth(url, 'GET',
+                                 'testname', 'testpass')
+
+        assert rv.status_code == 404
+        self.assert_html_response(rv)
+
+    def check_html_file_del(self, url, fileinfo, exists, parent_exists):
+        rv = self.open_with_auth(url, 'DELETE',
+                                 'testname', 'testpass')
+
+        if exists:
+            assert rv.status_code == 204
+        else:
+            assert rv.status_code == 404
+        self.assert_html_response(rv)
+
+    def check_html_folder_del(self, url, dirinfo, exists, parent_exists):
+        if url[-1] != '/':
+            rv = self.open_with_auth(url, 'DELETE',
+                                     'testname', 'testpass')
+            assert (rv.headers.get('Location') ==
+                    'http://localhost%s/' % url)
+            self.assert_html_response(rv)
+        else:
+            rv = self.open_with_auth(url, 'DELETE',
+                                     'testname', 'testpass')
+
+            assert rv.status_code == 204
+            self.assert_html_response(rv)
+
+    def check_html_folder_del_404(self, url, dirinfo, exists, parent_exists):
+        rv = self.open_with_auth(url, 'DELETE',
+                                 'testname', 'testpass')
+
+        assert rv.status_code == 404
+        self.assert_html_response(rv)
+
+    def check_html_file_put(self, url, fileinfo, exists, parent_exists):
+        rv = self.open_with_auth(url, 'PUT',
                                  'testname', 'testpass',
-                                 data='abcdefg')
+                                 data=fileinfo['content'])
 
-        assert rv.status_code == 201
-        assert rv.content_type.startswith('text/html')
-        assert rv.mimetype == 'text/html'
+        if exists:
+            assert rv.status_code == 409
+        elif not parent_exists:
+            assert rv.status_code == 404
+        else:
+            assert rv.status_code == 201
+        self.assert_html_response(rv)
 
-    def test_html_file_put_404(self):
-        rv = self.open_with_auth('/newfolder/newfile', 'PUT',
-                                 'testname', 'testpass',
-                                 data='abcdefg')
-
-        assert rv.status_code == 404
-        assert rv.content_type.startswith('text/html')
-        assert rv.mimetype == 'text/html'
-
-    def test_html_file_put_409(self):
-        rv = self.open_with_auth('/testfile', 'PUT',
-                                 'testname', 'testpass',
-                                 data='abcdefg')
-
-        assert rv.status_code == 409
-        assert rv.content_type.startswith('text/html')
-        assert rv.mimetype == 'text/html'
-
-    def test_html_file_delete(self):
-        rv = self.open_with_auth('/testfile', 'DELETE',
+    def check_html_folder_put(self, url, dirinfo, exists, parent_exists):
+        rv = self.open_with_auth(url, 'PUT',
                                  'testname', 'testpass')
 
-        assert rv.status_code == 204
-        assert rv.content_type.startswith('text/html')
-        assert rv.mimetype == 'text/html'
-
-    def test_html_file_delete_404(self):
-        rv = self.open_with_auth('/nonfile', 'DELETE',
-                                 'testname', 'testpass')
-
-        assert rv.status_code == 404
-        assert rv.content_type.startswith('text/html')
-        assert rv.mimetype == 'text/html'
+        if exists:
+            assert rv.status_code == 409
+        elif not parent_exists:
+            assert rv.status_code == 404
+        else:
+            assert rv.status_code == 201
+        self.assert_html_response(rv)
