4916e640c7b863b7aa58b8d7bacc9cfaddb769d8
Martin Hellmich <mhellmic@gmail.com>
2014-04-01 13:36:05 +0200

    refactor the tests into separate files
    central to all is the test_common module that
    provides functions to create files and users to test
    and to populate the backends.
    
    there are:
    storage_interface_tests: defining the interface of the storage layer
    storage_integration_tests: testing thw whole stack for the storage
      functionality from an http client point of view
    register_integration_tests: client-like testing for the registering
      functions
    
    The tests have just been restructured, no new things have been added.
    The storage integration tests have been refactored to use the
    resource object just like the storage interface tests --
    no semantic changes.
    
    Also changed the .travis.yml to run the new test set.

---
+++ test/storage_interface_tests.py
@@ -0,0 +1,479 @@
+from itertools import product
+from operator import add
+import os
+import tempfile
+
+from mock import patch
+from nose.tools import assert_raises
+
+from eudat_http_api import create_app
+
+from test.test_common import get_local_url_list, get_irods_url_list
+from test.test_common import get_user_list
+from test.test_common import create_local_urls, create_irods_urls
+from test.test_common import erase_local_urls, erase_irods_urls
+
+
+DB_FD, DB_FILENAME = tempfile.mkstemp()
+SQLALCHEMY_DATABASE_URI = '%s%s' % ('sqlite:///', DB_FILENAME)
+DEBUG = True
+TESTING = True
+STORAGE = 'local'
+
+
+class TestStorageApi:
+    url_list = None
+
+    from collections import namedtuple
+    Auth = namedtuple('Auth', 'username password')
+
+    @classmethod
+    def setup_class(cls):
+        config = os.getenv('TEST_CONFIG')
+        if config is not None:
+            app = create_app(config)
+        else:
+            app = create_app(__name__)
+
+        if app.config['STORAGE'] == 'local':
+            cls.url_list = get_local_url_list()
+        elif app.config['STORAGE'] == 'irods':
+            with app.app_context():
+                cls.url_list = get_irods_url_list(app.config['RODSZONE'])
+
+    def setup(self):
+        # this is needed to give each test
+        # its own app
+        config = os.getenv('TEST_CONFIG')
+        if config is not None:
+            app = create_app(config)
+        else:
+            app = create_app(__name__)
+
+        self.app = app
+        self.client = app.test_client()
+
+        self.storage_config = app.config['STORAGE']
+
+        if self.app.config['STORAGE'] == 'local':
+            create_local_urls(self.url_list)
+        elif self.app.config['STORAGE'] == 'irods':
+            self.irods_config = (self.app.config['RODSHOST'],
+                                 self.app.config['RODSPORT'],
+                                 self.app.config['RODSZONE']
+                                 )
+            with self.app.app_context():
+                create_irods_urls(self.url_list,
+                                  self.irods_config)
+
+    def teardown(self):
+        if self.app.config['STORAGE'] == 'local':
+            erase_local_urls(self.url_list)
+        elif self.app.config['STORAGE'] == 'irods':
+            with self.app.app_context():
+                erase_irods_urls(self.url_list,
+                                 self.irods_config)
+
+    def check_storage(self, check_func):
+        for (resource,
+             userinfo) in product(self.url_list,
+                                  get_user_list()):
+            yield (check_func,
+                   {
+                       'resource': resource,
+                       'userinfo': userinfo,
+                   })
+
+    def test_auth(self):
+        for t in self.check_storage(self.check_auth):
+            yield t
+
+    def test_stat(self):
+        for t in self.check_storage(self.check_stat):
+            yield t
+
+    def test_read(self):
+        for t in self.check_storage(self.check_read):
+            yield t
+
+    def test_ls(self):
+        for t in self.check_storage(self.check_ls):
+            yield t
+
+    def test_mkdir(self):
+        for t in self.check_storage(self.check_mkdir):
+            yield t
+
+    def test_rmdir(self):
+        for t in self.check_storage(self.check_rmdir):
+            yield t
+
+    def test_rm(self):
+        for t in self.check_storage(self.check_rm):
+            yield t
+
+    def test_write(self):
+        for t in self.check_storage(self.check_write):
+            yield t
+
+    def check_auth(self, params):
+        with self.app.test_request_context():
+            from eudat_http_api.http_storage import storage
+            userinfo = params['userinfo']
+
+            rv = storage.authenticate(userinfo.name,
+                                      userinfo.password)
+
+            if userinfo.valid:
+                assert rv is True
+            else:
+                assert rv is False
+
+    def check_stat(self, params):
+        if params['resource'].exists and params['userinfo'].valid:
+            self.check_stat_good(**params)
+        else:
+            self.check_stat_except(**params)
+
+    def check_stat_good(self, resource, userinfo):
+        with self.app.test_request_context(), \
+                patch(
+                'eudat_http_api.http_storage.%sstorage._get_authentication'
+                % self.storage_config,
+                return_value=self.Auth(userinfo.name, userinfo.password)):
+
+            from eudat_http_api.http_storage import storage
+
+            rv = storage.stat(resource.path)
+
+            assert hasattr(rv, '__iter__')
+            if resource.is_dir():
+                assert rv['type'] == storage.DIR
+                assert 'children' in rv
+                if 'children' in rv:
+                    assert rv['children'] == resource.objinfo['children']
+            elif resource.is_file():
+                assert rv['type'] == storage.FILE
+                assert 'size' in rv
+                if 'size' in rv:
+                    assert rv['size'] == resource.objinfo['size']
+
+            assert 'user_metadata' not in rv
+
+            # check for a request with metadata disabled
+            rv = storage.stat(resource.path, None)
+
+            assert hasattr(rv, '__iter__')
+            if resource.is_dir():
+                assert 'children' in rv
+                if 'children' in rv:
+                    assert rv['children'] == resource.objinfo['children']
+            elif resource.is_file():
+                assert 'size' in rv
+                if 'size' in rv:
+                    assert rv['size'] == resource.objinfo['size']
+
+            assert 'user_metadata' not in rv
+
+            # check for a request with metadata
+            rv = storage.stat(resource.path, True)
+
+            assert hasattr(rv, '__iter__')
+            if resource.is_dir():
+                assert 'children' in rv
+                if 'children' in rv:
+                    assert rv['children'] == resource.objinfo['children']
+            elif resource.is_file():
+                assert 'size' in rv
+                if 'size' in rv:
+                    assert rv['size'] == resource.objinfo['size']
+
+            assert 'user_metadata' in rv
+            if 'user_metadata' in rv:
+                assert len(rv['user_metadata']) == 0
+
+    def check_stat_except(self, resource, userinfo):
+        with self.app.test_request_context(), \
+                patch(
+                'eudat_http_api.http_storage.%sstorage._get_authentication'
+                % self.storage_config,
+                return_value=self.Auth(userinfo.name, userinfo.password)):
+            from eudat_http_api.http_storage import storage
+
+            if userinfo.valid:
+                assert_raises(storage.NotFoundException,
+                              storage.stat,
+                              resource.path)
+            else:
+                assert_raises(storage.NotAuthorizedException,
+                              storage.stat,
+                              resource.path)
+
+    def check_read(self, params):
+        if (params['resource'].exists and params['resource'].is_file() and
+                params['userinfo'].valid):
+            self.check_read_good(**params)
+        else:
+            self.check_read_except(**params)
+
+    def check_read_good(self, resource, userinfo):
+        with self.app.test_request_context(), \
+                patch(
+                'eudat_http_api.http_storage.%sstorage._get_authentication'
+                % self.storage_config,
+                return_value=self.Auth(userinfo.name, userinfo.password)):
+
+            from eudat_http_api.http_storage import storage
+
+            gen, fsize, contentlen, rangelist = storage.read(resource.path)
+
+            data = reduce(add, map(lambda (a, b, c, d): d, gen))
+            assert data == resource.objinfo['content']
+            assert fsize == contentlen
+            assert fsize == resource.objinfo['size']
+            assert rangelist == []
+
+            single_range = [[1, 4]]
+            gen, fsize, contentlen, rangelist = storage.read(resource.path,
+                                                             single_range)
+
+            data = reduce(add, map(lambda (a, b, c, d): d, gen))
+            assert data == resource.objinfo['content'][1:4+1]
+            assert fsize == resource.objinfo['size']
+            if resource.objinfo['size'] >= 4:
+                assert contentlen == 4
+                assert rangelist == [(1, 4)]
+            else:
+                assert contentlen == resource.objinfo['size'] - 1  # no char 0
+                assert rangelist == [(1, resource.objinfo['size'])]
+
+    def check_read_except(self, resource, userinfo):
+        with self.app.test_request_context(), \
+                patch(
+                'eudat_http_api.http_storage.%sstorage._get_authentication'
+                % self.storage_config,
+                return_value=self.Auth(userinfo.name, userinfo.password)):
+
+            from eudat_http_api.http_storage import storage
+
+            if resource.is_dir() and resource.exists and userinfo.valid:
+                assert_raises(storage.IsDirException,
+                              storage.read,
+                              resource.path)
+            elif resource.is_file() and not resource.exists and userinfo.valid:
+                assert_raises(storage.NotFoundException,
+                              storage.read,
+                              resource.path)
+            elif not userinfo.valid:
+                assert_raises(storage.NotAuthorizedException,
+                              storage.read,
+                              resource.path)
+
+    def check_ls(self, params):
+        if (params['resource'].exists and params['userinfo'].valid and
+                params['resource'].is_dir()):
+            self.check_ls_good(**params)
+        else:
+            self.check_ls_except(**params)
+
+    def check_ls_good(self, resource, userinfo):
+        with self.app.test_request_context(), \
+                patch(
+                'eudat_http_api.http_storage.%sstorage._get_authentication'
+                % self.storage_config,
+                return_value=self.Auth(userinfo.name, userinfo.password)):
+
+            from eudat_http_api.http_storage import storage
+
+            ls_gen = storage.ls(resource.path)
+
+            ls_res = list(ls_gen)
+            assert len(ls_res) == resource.objinfo['children']
+            ls_names = map(lambda x: x.name, ls_res)
+            assert set(ls_names) == set(resource.objinfo['children_names'])
+
+    def check_ls_except(self, resource, userinfo):
+        with self.app.test_request_context(), \
+                patch(
+                'eudat_http_api.http_storage.%sstorage._get_authentication'
+                % self.storage_config,
+                return_value=self.Auth(userinfo.name, userinfo.password)):
+
+            from eudat_http_api.http_storage import storage
+
+            if not userinfo.valid:
+                assert_raises(storage.NotAuthorizedException,
+                              storage.ls, resource.path)
+            elif not resource.exists:
+                assert_raises(storage.NotFoundException,
+                              storage.ls, resource.path)
+            elif not resource.is_dir():
+                assert_raises(storage.IsFileException,
+                              storage.ls, resource.path)
+
+    def check_mkdir(self, params):
+        # there is no distinction between files and dirs.
+        # filenames are also used to create directories.
+        if (not params['resource'].exists and params['resource'].parent_exists
+                and params['userinfo'].valid):
+            self.check_mkdir_good(**params)
+        else:
+            self.check_mkdir_except(**params)
+
+    def check_mkdir_good(self, resource, userinfo):
+        with self.app.test_request_context(), \
+                patch(
+                'eudat_http_api.http_storage.%sstorage._get_authentication'
+                % self.storage_config,
+                return_value=self.Auth(userinfo.name, userinfo.password)):
+
+            from eudat_http_api.http_storage import storage
+
+            storage.mkdir(resource.path)
+
+    def check_mkdir_except(self, resource, userinfo):
+        with self.app.test_request_context(), \
+                patch(
+                'eudat_http_api.http_storage.%sstorage._get_authentication'
+                % self.storage_config,
+                return_value=self.Auth(userinfo.name, userinfo.password)):
+
+            from eudat_http_api.http_storage import storage
+
+            if not userinfo.valid:
+                assert_raises(storage.NotAuthorizedException,
+                              storage.mkdir, resource.path)
+            elif resource.exists:
+                assert_raises(storage.ConflictException,
+                              storage.mkdir, resource.path)
+            elif not resource.parent_exists:
+                assert_raises(storage.NotFoundException,
+                              storage.mkdir, resource.path)
+
+    def check_rmdir(self, params):
+        if (params['resource'].exists and params['userinfo'].valid and
+                params['resource'].is_dir() and
+                params['resource'].objinfo['children'] == 0):
+            self.check_rmdir_good(**params)
+        else:
+            self.check_rmdir_except(**params)
+
+    def check_rmdir_good(self, resource, userinfo):
+        with self.app.test_request_context(), \
+                patch(
+                'eudat_http_api.http_storage.%sstorage._get_authentication'
+                % self.storage_config,
+                return_value=self.Auth(userinfo.name, userinfo.password)):
+
+            from eudat_http_api.http_storage import storage
+
+            storage.rmdir(resource.path)
+
+    def check_rmdir_except(self, resource, userinfo):
+        with self.app.test_request_context(), \
+                patch(
+                'eudat_http_api.http_storage.%sstorage._get_authentication'
+                % self.storage_config,
+                return_value=self.Auth(userinfo.name, userinfo.password)):
+
+            from eudat_http_api.http_storage import storage
+
+            if not userinfo.valid:
+                assert_raises(storage.NotAuthorizedException,
+                              storage.rmdir, resource.path)
+            elif (resource.exists and resource.is_dir() and
+                    resource.objinfo['children'] > 0):
+                assert_raises(storage.ConflictException,
+                              storage.rmdir, resource.path)
+            elif not resource.exists:
+                assert_raises(storage.NotFoundException,
+                              storage.rmdir, resource.path)
+            elif resource.is_file():
+                assert_raises(storage.IsFileException,
+                              storage.rmdir, resource.path)
+
+    def check_rm(self, params):
+        if (params['resource'].exists and params['userinfo'].valid and
+                params['resource'].is_file()):
+            self.check_rm_good(**params)
+        else:
+            self.check_rm_except(**params)
+
+    def check_rm_good(self, resource, userinfo):
+        with self.app.test_request_context(), \
+                patch(
+                'eudat_http_api.http_storage.%sstorage._get_authentication'
+                % self.storage_config,
+                return_value=self.Auth(userinfo.name, userinfo.password)):
+
+            from eudat_http_api.http_storage import storage
+
+            storage.rm(resource.path)
+
+    def check_rm_except(self, resource, userinfo):
+        with self.app.test_request_context(), \
+                patch(
+                'eudat_http_api.http_storage.%sstorage._get_authentication'
+                % self.storage_config,
+                return_value=self.Auth(userinfo.name, userinfo.password)):
+
+            from eudat_http_api.http_storage import storage
+
+            if not userinfo.valid:
+                assert_raises(storage.NotAuthorizedException,
+                              storage.rm, resource.path)
+            elif not resource.exists:
+                assert_raises(storage.NotFoundException,
+                              storage.rm, resource.path)
+            elif resource.is_dir():
+                assert_raises(storage.IsDirException,
+                              storage.rm, resource.path)
+
+    def check_write(self, params):
+        if (not params['resource'].exists and params['resource'].parent_exists
+                and params['userinfo'].valid and params['resource'].is_file()):
+            self.check_write_good(**params)
+        elif not params['resource'].is_dir():
+            self.check_write_except(**params)
+
+    def check_write_good(self, resource, userinfo):
+        with self.app.test_request_context(), \
+                patch(
+                'eudat_http_api.http_storage.%sstorage._get_authentication'
+                % self.storage_config,
+                return_value=self.Auth(userinfo.name, userinfo.password)):
+
+            from eudat_http_api.http_storage import storage
+            file_content = resource.objinfo['content']
+            chunk_size = 5
+
+            write_gen = (file_content[i:i+chunk_size]
+                         for i in xrange(0, len(file_content), chunk_size))
+
+            write_count = storage.write(resource.path, write_gen)
+
+            assert write_count == len(file_content)
+
+    def check_write_except(self, resource, userinfo):
+        with self.app.test_request_context(), \
+                patch(
+                'eudat_http_api.http_storage.%sstorage._get_authentication'
+                % self.storage_config,
+                return_value=self.Auth(userinfo.name, userinfo.password)):
+
+            from eudat_http_api.http_storage import storage
+            file_content = resource.objinfo['content']
+            chunk_size = 5
+
+            write_gen = (file_content[i:i+chunk_size]
+                         for i in xrange(0, len(file_content), chunk_size))
+
+            if not userinfo.valid:
+                assert_raises(storage.NotAuthorizedException,
+                              storage.write, resource.path, write_gen)
+            elif resource.exists:
+                assert_raises(storage.ConflictException,
+                              storage.write, resource.path, write_gen)
+            elif not resource.parent_exists:
+                assert_raises(storage.NotFoundException,
+                              storage.write, resource.path, write_gen)
---
+++ test/test_common.py
@@ -0,0 +1,231 @@
+from __future__ import with_statement
+from collections import namedtuple
+import os
+import shutil
+
+
+class RestResource:
+    ContainerType = 'dir'
+    FileType = 'file'
+
+    url = None
+    path = None  # for now path and url are the same
+    objtype = None
+    objinfo = {}
+    exists = None
+    parent_exists = None
+
+    def __init__(self, url,
+                 objtype,
+                 objinfo,
+                 exists=True,
+                 parent_exists=True):
+
+        self.url = url
+        self.objtype = objtype
+        self.objinfo = objinfo
+        self.exists = exists
+        self.parent_exists = parent_exists
+        self.path = self.url
+
+    def is_dir(self):
+        return self.objtype == self.ContainerType
+
+    def is_file(self):
+        return self.objtype == self.FileType
+
+    def __str__(self):
+        return ('path: %s; type: %s; exists: %s; parent_exists: %s'
+                % (self.path,
+                   self.objtype,
+                   self.exists,
+                   self.parent_exists)
+                )
+
+    def __repr__(self):
+        return self.__str__()
+
+
+def get_url_list():
+    l = [
+        # existing objects first
+        RestResource('/', RestResource.ContainerType, {
+            'children': 3,
+            'children_names': ['testfile', 'testfolder', 'emptyfolder']
+        }, True, True),
+        RestResource('/testfile', RestResource.FileType, {
+            'size': 3,
+            'content': 'abc',
+        }, True, True),
+        RestResource('/testfolder', RestResource.ContainerType, {
+            'children': 1,
+            'children_names': ['testfile']
+        }, True, True),
+        RestResource('/testfolder/', RestResource.ContainerType, {
+            'children': 1,
+            'children_names': ['testfile']
+        }, True, True),
+        RestResource('/testfolder/testfile', RestResource.FileType, {
+            'size': 26,
+            'content': 'abcdefghijklmnopqrstuvwxyz',
+        }, True, True),
+        RestResource('/emptyfolder', RestResource.ContainerType, {
+            'children': 0,
+            'children_names': []
+        }, True, True),
+        RestResource('/emptyfolder/', RestResource.ContainerType, {
+            'children': 0,
+            'children_names': []
+        }, True, True),
+        # not existing objects here
+        RestResource('/nonfolder', RestResource.ContainerType, {},
+                     False, True),
+        RestResource('/testfolder/nonfolder', RestResource.ContainerType, {},
+                     False, True),
+        RestResource('/nonofile', RestResource.FileType, {
+            'size': 10,
+            'content': '1234567890',
+        }, False, True),
+        RestResource('/wrongfilesizefile', RestResource.FileType, {
+            'size': 4444,
+            'content': '1234567890',
+        }, False, True),
+        RestResource('/emptyfile', RestResource.FileType, {
+            'size': 0,
+            'content': '',
+        }, False, True),
+        # non-existing with non-existing parent here
+        RestResource('/newfolder/newfile', RestResource.FileType, {
+            'size': 10,
+            'content': '1234567890',
+        }, False, False),
+    ]
+    #print 'Testing %d different URLs' % len(l)
+    return l
+
+
+def get_local_url_list():
+    l = []
+    for o in get_url_list():
+        o.path = '/tmp/new%s' % o.path
+        l.append(o)
+
+    return l
+
+
+def get_irods_url_list(rodszone):
+    l = []
+    for user in [u for u in get_user_list() if u.valid]:
+        for o in get_url_list():
+            o.path = '/%s/home/%s%s' % (rodszone, user.name, o.path)
+            l.append(o)
+
+    return l
+
+
+def get_user_list():
+    User = namedtuple('User', 'name password valid')
+    l = [
+        User('testname', 'testpass', True),
+        User('testname', 'notvalid', False),
+        User('notvalidname', 'notvalid', False),
+    ]
+    #print 'Testing %d different users' % len(l)
+    return l
+
+
+def create_local_urls(url_list):
+    for obj in [o for o in url_list if o.exists]:
+        if obj.objtype == obj.ContainerType:
+            try:
+                os.makedirs(obj.path)
+            except OSError:
+                pass
+        elif obj.objtype == obj.FileType:
+            try:
+                os.makedirs(os.path.split(obj.path)[0])
+            except OSError:
+                pass
+            with open(obj.path, 'wb') as f:
+                f.write(obj.objinfo['content'])
+
+
+def create_irods_connection(username, password, rodsconfig):
+    from irods import (getRodsEnv, rcConnect, clientLoginWithPassword,
+                       rodsErrorName)
+
+    err, rodsEnv = getRodsEnv()  # Override all values later
+    rodsEnv.rodsUserName = username
+
+    rodsEnv.rodsHost = rodsconfig[0]
+    rodsEnv.rodsPort = rodsconfig[1]
+    rodsEnv.rodsZone = rodsconfig[2]
+
+    conn, err = rcConnect(rodsEnv.rodsHost,
+                          rodsEnv.rodsPort,
+                          rodsEnv.rodsUserName,
+                          rodsEnv.rodsZone
+                          )
+
+    if err.status != 0:
+        raise Exception('Connecting to iRODS failed %s'
+                        % rodsErrorName(err.status)[0])
+
+    err = clientLoginWithPassword(conn, password)
+
+    if err != 0:
+        raise Exception('Authenticating to iRODS failed %s, user: %, pw: %s'
+                        % rodsErrorName(err.status)[0], username, password)
+
+    return conn
+
+
+def create_irods_urls(url_list, rodsconfig):
+    from irods import irodsCollection, irodsOpen
+
+    for user in [u for u in get_user_list() if u.valid]:
+        conn = create_irods_connection(user.name, user.password, rodsconfig)
+        for obj in [o for o in url_list if o.exists]:
+            if obj.objtype == obj.ContainerType:
+                base, name = os.path.split(obj.path)
+                coll = irodsCollection(conn, base)
+                coll.createCollection(name)
+            elif obj.objtype == obj.FileType:
+                coll = irodsCollection(conn)
+                coll.createCollection(os.path.split(obj.path)[0])
+                file_handle = irodsOpen(conn, obj.path, 'w')
+                if file_handle is not None:
+                    file_handle.write(obj.objinfo['content'])
+                    file_handle.close()
+        conn.disconnect()
+
+
+def erase_local_urls(url_list):
+    for obj in url_list:
+        if obj.objtype == obj.ContainerType:
+            try:
+                shutil.rmtree(obj.path, ignore_errors=True)
+            except OSError:
+                raise
+        elif obj.objtype == obj.FileType:
+            try:
+                os.remove(obj.path)
+            except OSError:
+                pass
+
+
+def erase_irods_urls(url_list, rodsconfig):
+    from irods import irodsOpen, irodsCollection
+
+    for user in [u for u in get_user_list() if u.valid]:
+        conn = create_irods_connection(user.name, user.password, rodsconfig)
+        for obj in url_list:
+            file_handle = irodsOpen(conn, obj.path, 'r')
+            if file_handle is not None:
+                file_handle.close()
+                file_handle.delete(force=True)
+
+            base, name = os.path.split(obj.path)
+            coll = irodsCollection(conn, base)
+            coll.deleteCollection(name)
+        conn.disconnect()
