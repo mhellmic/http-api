507445122bc736da2d22eb9e836b4dccfaccb009
Martin Hellmich <mhellmic@gmail.com>
2014-04-10 14:04:20 +0200

    use my pyrods version for now
    this version hosted in my github account has a fix related to
    memory leaks.
    it could be transferred to the eudat account if we decide
    to use it for releases.

--- install_pyrods.sh
+++ install_pyrods.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 set -ex
 cd /tmp
-wget https://irodspython.googlecode.com/git/Downloads/PyRods-3.3.4.tar.gz -O /tmp/PyRods-3.3.4.tar.gz
-tar xvzf PyRods-3.3.4.tar.gz
-cd PyRods-3.3.4
+
+#wget https://irodspython.googlecode.com/git/Downloads/PyRods-3.3.4.tar.gz -O /tmp/PyRods-3.3.4.tar.gz
+#tar xvzf PyRods-3.3.4.tar.gz
+#cd PyRods-3.3.4
+
+wget https://github.com/mhellmic/irodspython/archive/master.zip -O /tmp/PyRods.zip
+unzip PyRods.zip
+cd irodspython-master/PyRods
+
 export CFLAGS=-fPIC
 ./scripts/configure
 make clients
