361357eea974b0a937d4bcec6d8adce4e0ba226e
Martin Hellmich <mhellmic@gmail.com>
2014-04-15 16:28:44 +0200

    many fixes to cdmi output
    - add output buffering (experimental)
    - add cdmi version recognition support
    - change to adjust to changed request_wants functions
    - add get_config_parameter function to get CDMI params
    - add function to immediately reject a non-conform cdmi request
    - immediately disallow cdmi requests with multi range
    - fix valuerange parameter for range request
    - return 403 (not 401) if credentials are wrong
    - return correct cdmi version and content_type on GET functions
    - fix line lengths
    - use json.dumps() to prepare values for json output wherever possible
    - add missing cdmi values
    - add 403 error handler

--- eudat_http_api/http_storage/init.py
+++ eudat_http_api/http_storage/init.py
@@ -12,6 +12,7 @@ http_storage = Blueprint('http_storage', __name__,
 @http_storage.route('/', methods=['GET'], defaults={'objpath': '/'})
 @http_storage.route('/<path:objpath>', methods=['GET'])
 @auth.requires_auth
+@cdmi.check_cdmi
 def get_cdmi_obj(objpath='/'):
     absolute_objpath = cdmi.make_absolute_path(objpath)
     if absolute_objpath[-1] == '/':
@@ -23,6 +24,7 @@ def get_cdmi_obj(objpath='/'):
 @http_storage.route('/', methods=['PUT'], defaults={'objpath': '/'})
 @http_storage.route('/<path:objpath>', methods=['PUT'])
 @auth.requires_auth
+@cdmi.check_cdmi
 def put_cdmi_obj(objpath):
     absolute_objpath = cdmi.make_absolute_path(objpath)
     if absolute_objpath[-1] == '/':
@@ -34,12 +36,18 @@ def put_cdmi_obj(objpath):
 @http_storage.route('/', methods=['DELETE'], defaults={'objpath': '/'})
 @http_storage.route('/<path:objpath>', methods=['DELETE'])
 @auth.requires_auth
+@cdmi.check_cdmi
 def del_cdmi_obj(objpath):
     absolute_objpath = cdmi.make_absolute_path(objpath)
     if absolute_objpath[-1] == '/':
         return cdmi.del_cdmi_dir_obj(absolute_objpath)
     else:
         return cdmi.del_cdmi_file_obj(absolute_objpath)
+
+
+@http_storage.errorhandler(403)
+def not_authorized_handler(e):
+    return cdmi.not_authorized_handler(e)
 
 
 @http_storage.teardown_request
