c4d90d3b4ed4e7148d7d1100518cf17c2dc0f900
Martin Hellmich <mhellmic@gmail.com>
2014-04-15 16:13:12 +0200

    fix/refactor requests functions
    - make cdmi match both -object and -container
    - add convenience request_wants_cdmi function
    - fix line lengths

--- eudat_http_api/common.py
+++ eudat_http_api/common.py
@@ -24,15 +24,21 @@ def add_trailing_slash(path):
     except TypeError:
         return path
 
-class ContentTypes:
-    json, cdmi_object = ('application/json', 'application/cdmi-object')
 
-# in long term move to Flask-Negotiate?
+class ContentTypes:
+    json = 'application/json'
+    cdmi = ('application/cdmi-object', 'application/cdmi-container')
+
 
 def request_wants(content_type):
     best = request.accept_mimetypes.best_match([content_type, 'text/html'])
-    return best == content_type and request.accept_mimetypes[best] > request.accept_mimetypes['text/html']
+    return (best == content_type and request.accept_mimetypes[best]
+            > request.accept_mimetypes['text/html'])
+
 
 def request_wants_json():
     return request_wants(ContentTypes.json)
 
+
+def request_wants_cdmi():
+    return any(map(request_wants, ContentTypes.cdmi))
