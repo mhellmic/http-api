02b4e03f2532d269ad5bfb91906bf294bb3e595d
Martin Hellmich <mhellmic@gmail.com>
2014-04-23 12:17:09 +0200

    fix demo notebook

--- demo_notebook.sh
+++ demo_notebook.sh
@@ -4,6 +4,7 @@ mkdir /tmp/http_server
 echo "Hello, I am a small testfile" > /tmp/http_server/testfile
 mkdir /tmp/http_server/testfolder
 echo "Hello, I am a small testfile in a nested directory" > /tmp/http_server/testfile
+mkdir /tmp/http_server/registered
 
 # create 100k dir
 # put h1big.root in /tmp
@@ -21,18 +22,18 @@ echo "Hello, I am a small testfile in a nested directory" > /tmp/http_server/tes
 curl -u testname:testpass http://localhost:5000/tmp/http_server/
 
 # 3. list folder with curl json
-curl -u testname:testpass -H 'accept: application/json' http://localhost:5000/tmp/http_server/
+#curl -u testname:testpass -H 'accept: application/json' http://localhost:5000/tmp/http_server/
 
 # 4. put file with curl, then list folder
-curl -X PUT -u testname:testpass -H 'accept: application/json' http://localhost:5000/tmp/h1big.root /tmp/http_server/h1big.root
-curl -u testname:testpass -H 'accept: application/json' http://localhost:5000/tmp/http_server/
+curl -X PUT -u testname:testpass -H 'accept: application/json' http://localhost:5000/tmp/http_server/h1big.root --upload-file /tmp/h1big.root
+curl -u testname:testpass http://localhost:5000/tmp/http_server/
 # go to http://localhost:5000/tmp/http_server
 
 # 5. get a folder with cdmi
-curl -u testname:testpass -H 'accept: application/cdmi-container' -H 'X-CDMI-Specification-Version: 1.0.2' http://localhost:5000/tmp/h1big.root /tmp/http_server/
+curl -u testname:testpass -H 'accept: application/cdmi-container' -H 'X-CDMI-Specification-Version: 1.0.2' http://localhost:5000/tmp/http_server/
 
 # 6. get a file with cdmi
-curl -u testname:testpass -H 'accept: application/cdmi-container' -H 'X-CDMI-Specification-Version: 1.0.2' http://localhost:5000/tmp/h1big.root /tmp/http_server/h1big.root
+curl -u testname:testpass -H 'accept: application/cdmi-container' -H 'X-CDMI-Specification-Version: 1.0.2' http://localhost:5000/tmp/http_server/testfile
 
 
 ## Registering workflow
@@ -57,3 +58,6 @@ curl -L -H 'accept: application/cdmi-object' -H 'X-CDMI-Specification-Version: 1
 
 # 9. Register from the command line
 curl -X POST -u testname:testpass -H 'accept: application/json' http://localhost:5000/request/ --data 'src_url=http://localhost:5000/tmp/http_server/h1big.root'
+
+# 10. Look at the request from the command line
+curl -u testname:testpass -H 'accept: application/json'  http://localhost:5000/request/REQUESTID
