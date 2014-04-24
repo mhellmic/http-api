ec1c32f88c5840a26bef0adfcd12c49002ac71c3
Martin Hellmich <mhellmic@gmail.com>
2014-04-24 09:32:31 +0200

    add cdmi_copy to cdmiclient
    this is the PUT with a copy parameter
    
    Conflicts:
    	eudat_http_api/cdmiclient.py

--- eudat_http_api/cdmiclient.py
+++ eudat_http_api/cdmiclient.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from flask import json
 import requests
 
 
@@ -30,3 +33,27 @@ class CDMIClient:
         }
 
         return requests.put(url, headers=headers, data=data, auth=self.auth)
+
+    def cdmi_copy(self, url, src_url):
+        cdmi_headers = {
+            'Accept': 'application/cdmi-object',
+            'Content-type': 'application/cdmi-object',
+            'X-CDMI-Specification-Version': '1.0.2',
+        }
+
+        cdmi_data = json.dumps({
+            'copy': src_url,
+        })
+
+        return requests.put(url, headers=cdmi_headers,
+                            data=cdmi_data, auth=self.auth)
+
+    def cdmi_delete(self, url):
+        headers = {
+            'Accept': 'application/cdmi-object',
+            'X-CDMI-Specification-Version': '1.0.2',
+        }
+
+        r = requests.delete(url, headers=headers, auth=self.auth)
+
+        return r
