05310c5ba4a4a6875be2770aa07ac20a846f9de2
Martin Hellmich <mhellmic@gmail.com>
2014-04-29 15:46:49 +0200

    add ssl config to the gevent startup file
    If the ssl hostkey and -cert file are given, start the server with ssl
    support. By switching on ssl, plain http gets disabled.
    The port from the configuration file is used.

--- gevent_server.py
+++ gevent_server.py
@@ -3,5 +3,15 @@ from eudat_http_api import create_app
 
 app = create_app('config')
 
-http_server = WSGIServer(('', 5000), app)
+ssl_args = {}
+
+if (ssl_args['ssl_keyfile'] is not None and
+        ssl_args['ssl_certfile'] is not None):
+    ssl_args = {
+        'ssl_keyfile': app.config.get('SSL_KEY', None),
+        'ssl_certfile': app.config.get('SSL_CERT', None),
+        'ca_certs': app.config.get('SSL_CACERTS', None),
+    }
+
+http_server = WSGIServer(('', 5000), app, **ssl_args)
 http_server.serve_forever()
