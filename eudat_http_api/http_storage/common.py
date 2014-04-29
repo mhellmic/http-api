18f6499c03d2da4651edb486ff3f67999a1cdf6d
Martin Hellmich <mhellmic@gmail.com>
2014-04-29 11:55:01 +0200

    refactor PUT and DELETE methods
    split the put and delete methods between cdmi and noncdmi access just
    like the get methods. both functions do not support json output.

--- eudat_http_api/http_storage/common.py
+++ eudat_http_api/http_storage/common.py
@@ -29,3 +29,24 @@ def make_absolute_path(path):
         return '/%s' % path
     else:
         return '/'
+
+
+class StreamWrapper(object):
+    """Wrap the WSGI input so it doesn't store everything in memory.
+
+    taken from http://librelist.com/browser//flask/2011/9/9/any-way-to- \
+        stream-file-uploads/#d3f5efabeb0c20e24012605e83ce28ec
+
+    Apparently werkzeug needs a readline method, which I added with
+    the same implementation as read.
+    """
+    def __init__(self, stream):
+        self._stream = stream
+
+    def read(self, buffer_size):
+        rv = self._stream.read(buffer_size)
+        return rv
+
+    def readline(self, buffer_size):
+        rv = self._stream.read(buffer_size)
+        return rv
