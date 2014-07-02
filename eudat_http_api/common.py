1f65d68861dbf94e5990d84091c77426aafd248f
Martin Hellmich <mhellmic@gmail.com>
2014-07-02 13:40:36 +0200

    fixup create_path_links
    - make it faster by using an an aggregator in the loop
        every iteration the new subdir is added to the path
    - enable its use for paths of files
        don't assume a slash at the end and add the last entry
        accordingly

--- eudat_http_api/common.py
+++ eudat_http_api/common.py
@@ -1,17 +1,41 @@
 from __future__ import with_statement
-from collections import OrderedDict
 
+from collections import OrderedDict
 from flask import request
+import os
+
+from http_storage.common import add_trailing_slash
 
 
 def create_path_links(path):
+    """ Create links for each subdirectory in the path.
+
+    This function generates the full path for each subdirectory
+    in the given path, and stores it in an ordered dict with the
+    subdirectory's basename as key.
+    It works for directories or files at the end of the path.
+
+    This can be used to generate links to be displayed in a website.
+
+    Some caveats:
+    - if the path has a trailing slash, there will be a phantom empty
+        element after the split that we have to ignore.
+    - We assume absolute paths, so the first element is always empty.
+        Always replace it with a '/'.
+    - Intermediate directories must have a trailing slash, so we
+        add it manually.
+    """
     split = path.split('/')
     ret = OrderedDict()
-    for i in split[:-1]:
-        index = split.index(i)
-        if i == '':
-            i = '/'
-        ret[i] = '/'.join(split[:index + 1]) + '/'
+    agg = ''
+    split[0] = '/'
+    for item in split[:-1]:
+        agg = add_trailing_slash(os.path.join(agg, item))
+        ret[item] = agg
+    if split[-1] != '':
+        item = split[-1]
+        agg = os.path.join(agg, item)
+        ret[item] = agg
     return ret
 
 
