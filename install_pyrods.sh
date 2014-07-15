9e00151f83a0d36d52ae4746c8d7487047a6d0c4
Martin Hellmich <mhellmic@gmail.com>
2014-07-15 08:32:12 +0200

    use pyrods 3.3.5 (official) for testing

--- install_pyrods.sh
+++ install_pyrods.sh
@@ -2,13 +2,13 @@
 set -ex
 cd /tmp
 
-#wget https://irodspython.googlecode.com/git/Downloads/PyRods-3.3.4.tar.gz -O /tmp/PyRods-3.3.4.tar.gz
-#tar xvzf PyRods-3.3.4.tar.gz
-#cd PyRods-3.3.4
+wget https://irodspython.googlecode.com/git/Downloads/PyRods-3.3.5.tar.gz -O /tmp/PyRods-3.3.5.tar.gz
+tar xvzf PyRods-3.3.5.tar.gz
+cd PyRods-3.3.5
 
-wget https://github.com/mhellmic/irodspython/archive/master.zip -O /tmp/PyRods.zip
-unzip PyRods.zip
-cd irodspython-master/PyRods
+#wget https://github.com/mhellmic/irodspython/archive/master.zip -O /tmp/PyRods.zip
+#unzip PyRods.zip
+#cd irodspython-master/PyRods
 
 export CFLAGS=-fPIC
 ./scripts/configure
