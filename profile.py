265e900c5a0995b5539e931f744ce5c23d11bd80
Martin Hellmich <mhellmic@gmail.com>
2014-08-26 18:27:17 +0200

    update profile.py to use create_app

--- profile.py
+++ profile.py
@@ -1,9 +1,10 @@
 import sys
 from werkzeug.contrib.profiler import ProfilerMiddleware, MergeStream
-from eudat_http_api import app
+from eudat_http_api import create_app
 
+app = create_app('config')
 app.config['PROFILE'] = True
 f = open('profiler.log', 'w')
 stream = MergeStream(sys.stdout, f)
 app.wsgi_app = ProfilerMiddleware(app.wsgi_app, stream)
-app.run(debug = True)
+app.run(debug=True, host=app.config['HOST'], port=app.config['PORT'])
