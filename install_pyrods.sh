3c845808a413642d6a5e4542cffc7e029cc7b306
Martin Hellmich <mhellmic@gmail.com>
2014-09-09 18:52:17 +0200

    build and test with PyRods 3.3.6

--- install_pyrods.sh
+++ install_pyrods.sh
@@ -2,9 +2,9 @@
 set -ex
 cd /tmp
 
-wget https://irodspython.googlecode.com/git/Downloads/PyRods-3.3.5.tar.gz -O /tmp/PyRods-3.3.5.tar.gz
-tar xvzf PyRods-3.3.5.tar.gz
-cd PyRods-3.3.5
+wget https://irodspython.googlecode.com/git/Downloads/PyRods-3.3.6.tar.gz -O /tmp/PyRods-3.3.6.tar.gz
+tar xvzf PyRods-3.3.6.tar.gz
+cd PyRods-3.3.6
 
 #wget https://github.com/mhellmic/irodspython/archive/master.zip -O /tmp/PyRods.zip
 #unzip PyRods.zip
